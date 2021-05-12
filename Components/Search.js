ff9ca93972873adeab5458a759132903156dc93b
moudden1 <walid.moudden@ig2i.centralelille.fr>
2021-05-13 00:45:28 +0200

    Modif css container

--- Components/Search.js
+++ Components/Search.js
@@ -4,8 +4,8 @@ import { StyleSheet, View, TextInput, Button} from 'react-native'
 class Search extends React.Component {
     render(){
         return (
-            <View style={{marginTop: 50 }}>
-                <TextInput placeholder="Search ..." style={styles.textinput}/>
+            <View style={{styles.main_container}}>
+                <TextInput placeholder="search ..." style={styles.textinput}/>
                 <Button title="Search" onPress={() => {}}/>
             </View>
         )
@@ -13,6 +13,10 @@ class Search extends React.Component {
 }
 
 const styles = StyleSheet.create({
+    main_container:  {
+        flex: 1,
+        marginTop: 50
+    },
     textinput: {
         marginLeft: 5,
         marginRight: 5,
